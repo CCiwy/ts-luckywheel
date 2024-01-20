c1e2087d7aa0b8f515c8edf5e4fcac5d3be7a122
Ciwy <c.walter@afk-dev.de>
2024-01-20 16:51:16 +0100

    add: use React.FC for wheel slice

--- src/components/Wheel.tsx
+++ src/components/Wheel.tsx
@@ -16,15 +16,25 @@ function getCirclePoint(middle: Vec2, radius: number, alpha: number) {
     because in SVG 0 degress points to the right
     while in trigonometry 0 degrees points upward
     */
-    const radians = alpha * Math.PI / 180;
+    const radians = (alpha - 90) * Math.PI / 180;
     const result: Vec2 = {
         x: middle.x + radius * Math.cos(radians),
-        y: middle.y - radius * Math.sin(radians)
+        y: middle.y + radius * Math.sin(radians)
         };
     return result;
 }
 
 
+    function createSliceText(m: Vec2, radius: number, value: string, angle: number) {
+        const p = getCirclePoint(m, radius, angle); // maybe use radius * 0.8
+        const textElem = document.createElementNS(SVG_NS, 'text');
+        textElem.textContent = value;
+        textElem.setAttribute('x', p.x.toString());
+        textElem.setAttribute('y', p.y.toString());
+        textElem.setAttribute('transform', `rotate(${-90 + angle} ${p.x} ${p.y})`);
+        textElem.setAttribute('fill', 'white');
+        return textElem;
+    }
 
 
 interface WheelProps {
@@ -37,12 +47,34 @@ interface SliceProps {
     radius: number;
     middle: Vec2;
     color: string;
+    sliceAngle: number;
     }
 
 
-const WheelSlice: React.FC<SliceProps> = () => {
+const WheelSlice: React.FC<SliceProps> = (props) => {
+    const middle = props.middle;
+    const radius = props.radius;
+    const alpha = props.offsetAngle;
+    const beta = alpha + props.sliceAngle;
+    const p1 = getCirclePoint(middle, radius, alpha);
+    const p2 = getCirclePoint(middle, radius, beta);
+    const  largeArcFlag = props.sliceAngle <= 180 ? '0' : '1';
 
-    return (<></>)
+    const d = `
+            M ${middle.x} ${middle.y}
+            L ${p1.x} ${p1.y}
+            A ${radius} ${radius} 0 ${largeArcFlag} 1 ${p2.x}, ${p2.y}
+            Z`;
+
+            //slice.setAttributeNS(null, 'stroke', 'black');
+            //slice.setAttributeNS(null, 'stroke-width', '2');
+            //slice.setAttributeNS(null, 'transform', `rotate(${-90 + alpha} ${m.x} ${m.y})`);
+            //parent?.appendChild(slice);
+            //const angleBetween = alpha + 0.5 * sliceAngle;
+            //const textElem = createSliceText(value, angleBetween);
+            //parent?.appendChild(slice); 
+            //parent?.appendChild(textElem); 
+    return (<path d={d} fill={props.color} stroke='black'></path>)
 }
 
 
@@ -51,73 +83,23 @@ const Wheel: React.FC<WheelProps> = () => {
     const m: Vec2 = {x: radius, y: radius}
     const parentIdent = 'wheel';
     const initalValues = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+    const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple', 'orange', 'brown'];
     const [values, setValues] = useState<string[]>(initalValues);
-   
-
-    function clearWheel() {
-        const parent = document.getElementById(parentIdent); 
-            if (parent !== null) {
-                parent.innerHTML = '';
-            }
-        
-    }
-
-    function createSliceText(value: string, angle: number) {
-        const p = getCirclePoint(m, radius, angle); // maybe use radius * 0.8
-        const textElem = document.createElementNS(SVG_NS, 'text');
-        textElem.textContent = value;
-        textElem.setAttribute('x', p.x.toString());
-        textElem.setAttribute('y', p.y.toString());
-        textElem.setAttribute('transform', `rotate(${-90 + angle} ${p.x} ${p.y})`);
-        textElem.setAttribute('fill', 'white');
-        return textElem;
-    }
-
-
-
-    function createSlices(sliceValues: string[]) {
-        const numSlice = sliceValues.length;
-        const parent = document.getElementById(parentIdent); 
-        const sliceAngle = 360 / numSlice;
-        
-        sliceValues.forEach((value, i) => {
-            const alpha = sliceAngle * i;
-            const p1 = getCirclePoint(m, radius, alpha);
-            const p2 = getCirclePoint(m, radius, alpha + sliceAngle);
-            const d = `
-                    M ${m.x} ${m.y}
-                    L ${p1.x} ${p1.y}
-                    A ${radius} ${radius} 0 1 1 ${p2.x} ${p2.y}
-                    Z`;
-
-            const slice = document.createElementNS(SVG_NS, 'path');
-            slice.setAttributeNS(null, 'd', d);
-            slice.setAttributeNS(null, 'fill', 'red');
-            slice.setAttributeNS(null, 'stroke', 'black');
-            slice.setAttributeNS(null, 'stroke-width', '2');
-            //slice.setAttributeNS(null, 'transform', `rotate(${-90 + alpha} ${m.x} ${m.y})`);
-            parent?.appendChild(slice);
-            const angleBetween = alpha + 0.5 * sliceAngle;
-            const textElem = createSliceText(value, angleBetween);
-            parent?.appendChild(slice); 
-            parent?.appendChild(textElem); 
-            })
-
-    }
-    
-    useEffect(() => {
-        clearWheel();
-        createSlices(values);
-    }, [values])
 
 
     return (
     <div>
         <h1>Spin the Wheel</h1>
-        <svg id={parentIdent} width={radius * 2} height={radius * 2}></svg>
-
+        <svg id={parentIdent} width={radius * 2} height={radius * 2}>
+            { values.map((value, i) => {
+                const sliceAngle = 360 / values.length;
+                const alpha = sliceAngle * i
+                return <WheelSlice key={i} value={value} offsetAngle={alpha} sliceAngle={sliceAngle} radius={radius} middle={m} color={colors[i]} />
+            })
+        }
+        </svg>
     </div>
-        );
+    );
 }
 
 

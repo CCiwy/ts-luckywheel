48346cd12035d45772daa9cb1ae6d2e52cacb384
Ciwy <c.walter@afk-dev.de>
2024-01-22 16:13:51 +0100

    add: basic Form to fill the Wheel

--- src/components/WheelForm.tsx
+++ src/components/WheelForm.tsx
@@ -1,32 +1,49 @@
+import React, { useState, useEffect } from 'react';
 
-const WheelInputForm: React.FC = () => {
-    const handleAction = (e: Event) => {
-        e.preventDefault();
-        console.log("handleAction");
+
+
+interface WheelFormProps {
+    callback: (values: string[]) => void;
     }
 
-    const handleAddField = (e: Event) => {
-        e.preventDefault();
-        console.log("handleAddField");
-        }
-    
-    const handleDelField = (e: Event) => {
-    e.preventDefault();
-    console.log("handleDel");
-        }
+//todO: make this dynamic
+const initalValues = {
+    f1: '',
+    f2: '',
+    f3: '',
+    f4: '',
+    f5: '',
+    f6: ''
+}
+
+const WheelForm: React.FC = (props) => {
+    const updateValues = props.callback;
+    const [values, setValues] = useState<Object>(initalValues);
+
+    useEffect(() => {
+        let _values: string[] = (Object.values(values) as string[]).filter((v) => v !== '');
+        updateValues(_values)
+
+    }, [values]);
+
+
 
 
     const handleChange = (e: Event) => {
-    console.log(e.target.value)
-        }
+        setValues({...values, [e.target.id]: e.target.value})
+    }
 
     return (<div>
         <h2>WHEEL INPUT FORM GOES HERE</h2>
         <form>
-        <button onClick={(e) =>handleDelField(e)}> REMOVE FIELD</button>
-        <input type="text" onChange={(e) => handleChange(e)} />
-        <button onClick={(e) =>handleAddField(e)}> NEW FIELD</button>
-        <button onClick={(e) =>handleAction(e)}> CLICK ME</button>
+        <input id="f1" type="text" onChange={(e) => handleChange(e)} />
+        <input id="f2" type="text" onChange={(e) => handleChange(e)} />
+        <input id="f3" type="text" onChange={(e) => handleChange(e)} />
+        <input id="f4" type="text" onChange={(e) => handleChange(e)} />
+        <input id="f5" type="text" onChange={(e) => handleChange(e)} />
+        <input id="f6" type="text" onChange={(e) => handleChange(e)} />
         </form>
     </div>)
 }
+
+export default WheelForm;
